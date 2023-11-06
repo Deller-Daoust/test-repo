3268c72d8c02cc642dea5071fe3bbbfeb1203fef
Jayden Deller-Daoust <jayden.dellerdaoust@gmail.com>
2023-11-06 14:47:05 -0500

    mychanges

--- My project (2)/Assets/testScript.cs
+++ My project (2)/Assets/testScript.cs
@@ -7,7 +7,7 @@ public class testScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Console.Writeline("test");
     }
 
     // Update is called once per frame
